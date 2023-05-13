d13162cbea2c8f79a3fb31a27642a99728409a34
Diego Rodriguez <dsrrdiego@hotmail.com>
2023-05-12 21:35:01 -0300

    mejorando

--- bolas.js
+++ bolas.js
@@ -5,9 +5,9 @@ class Bola{
             bolaNumero++;
             this.numero=bolaNumero%4;
             this.x=Math.floor(Math.random()*400);
-            this.radio=Math.floor(Math.random()*150)+20;
-            this.direccionX=Math.floor(Math.random()*7)-3;
-            this.sentido=1.1;
+            this.radio=Math.floor(Math.random()*80)+20;
+            this.direccionX=Math.floor(Math.random()*3)*Math.floor(Math.random()*4);
+            this.caida=10;
             this.y=10;
             this.yTope=10;
             this.div=document.createElement('div');
@@ -15,7 +15,6 @@ class Bola{
             document.body.appendChild(this.div);
             bolasEnElAire.push(this);
     }else{
-        console.log('entro');
         
         if (copia.radio>20){
             this.numero=copia.numero;
@@ -23,7 +22,7 @@ class Bola{
             this.y=copia.y;
             this.radio=copia.radio/2;
             this.direccionX=copia.direccionX*direccion;
-            this.sentido=copia.sentido;
+            this.caida=copia.caida;
             this.yTope=copia.yTope;
             this.div=document.createElement('div');
             this.div.classList.add('bola');
@@ -38,20 +37,41 @@ class Bola{
         if (this.x+this.radio>970) this.direccionX*=-1;
         if (this.x-this.radio<0) this.direccionX*=-1;
 
-
-        if (this.yTope<150){
-            console.log(this.yTope);
-            
-            if (this.y<this.yTope && this.sentido==0.95){
-                
-                this.sentido=1.05;
-                this.yTope+=15;
-            }
-            if (this.y>400-this.radio) this.sentido=0.95;
-            this.y=this.y*this.sentido;
-            this.x+=this.direccionX;
+        if (this.yTope+2*(this.radio)<400){
             this.colision();
+            
+            if (this.y+this.radio>400)  this.caida=-10//-Math.abs(this.caida);
+            
+            if (this.y-this.radio<this.yTope) this.caida=10//Math.abs(this.caida);
+                
+            this.y=this.y+this.caida;
+            this.yTope+=0.3;
+
+            this.x+=this.direccionX;
+
+        }else{
+            this.radio-=10;
+            if (this.radio<3){
+            const indice=bolasEnElAire.indexOf(this);
+            bolasEnElAire.splice(indice,1);
+            document.body.removeChild(this.div);
+            }
         }
+        // }
+
+        // if (this.yTope<150){
+        //     console.log(this.yTope);
+            
+        //     if (this.y<this.yTope && this.sentido==0.95){
+                
+        //         this.sentido=1.05;
+        //         this.yTope+=15;
+        //     }
+        //     if (this.y>400-this.radio) this.sentido=0.95;
+        //     this.y=this.y*this.sentido;
+        //     this.x+=this.direccionX;
+        //     this.colision();
+        // }
     }
 
     colision(){
@@ -63,12 +83,12 @@ class Bola{
                 const hipo=Math.sqrt(difX**2+difY**2);
                 
                 if (hipo<this.radio+bola.radio) {
-                    // console.log('colision');
                     
+                    // this.x+=this.radio*this.direccionX;
+                    // this.y+=this.radio*this.caida/10;
+
                     this.direccionX*=-1;
-                    bola.direccionX*=-1;
-                    if (this.sentido==0.95) this.sentido=1.05; else this.sentido=0.95
-                    if (bola.sentido==0.95) bola.sentido=1.05; else bola.sentido=0.95
+                    this.caida*=-1;
                     
                 }
             }
