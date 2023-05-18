dbbae3b62b56f98d1429058c80f7921992723f8f
Diego Rodriguez <dsrrdiego@hotmail.com>
2023-05-18 10:18:33 -0300

    reacomodado con setup

---
+++ setup.js
@@ -0,0 +1,11 @@
+//CONFIGURACION DEL JUEGO
+let nivel=1; //nivel inicial de dificultad
+const BANDERA_CADA=3000; // cada cuantos pixeles aparecera la bandera de pasar de nivel
+const COFRE_EN=1000; //pixeles en que aparecerà el cofre
+const COFRE_A_PARTIR_DE= 1000; 
+const VIDAS_INICIAL=3;
+const FLECHAS_INICIAL=5;
+const FLECHAS_POR_COFRE=5;
+const APARICION =5000; //aparecen objetos al azar cada ms
+let LIMITE_X = window.innerWidth;
+const LIMITE_Y=520; //horizonte en pixeles
